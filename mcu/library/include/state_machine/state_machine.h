b9d592b56c214e7b5e0fbc158c6a6471ef2fe865
yudai <yudai.yy0804@gmail.com>
2024-01-03 01:42:50 +0900

    feat: state_machineクラスを実装した

---
+++ mcu/library/include/state_machine/state_machine.h
@@ -0,0 +1,44 @@
+/**
+ * @file state_machine.h
+ * @brief
+ */
+
+#pragma once
+#include <functional>
+#include <stdint.h>
+#include <stdio.h>
+
+namespace state_machine {
+
+using state_t = uint8_t;
+
+struct State {
+  state_t main;
+  state_t sub;
+  std::function<void(void)> function;
+};
+
+class StateMachine {
+private:
+  State previous_state_;
+  State current_state_;
+  State next_state_;
+
+public:
+  StateMachine() {}
+  void changeState(State next, uint8_t is_printf = 1) {
+    previous_state_ = current_state_;
+    current_state_ = next_state_;
+    next_state_ = next;
+    if (is_printf) {
+      printf("main = %d, sub = %d\r\n", current_state_.main,
+             current_state_.sub);
+    }
+    current_state_.function();
+  }
+
+  State getPreviousState() { return previous_state_; }
+  State getCurrentState() { return current_state_; }
+  State getNextState() { return next_state_; }
+};
+} // namespace state_machine
